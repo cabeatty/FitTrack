fa73c1234f7e62afff9957b2992061461c4b56a8
ColePillars <cole_p7@yahoo.com>
2016-11-11 17:03:20 -0500

    Sleep constructor

--- src/FitTrack/Components/sleepTracker.java
+++ src/FitTrack/Components/sleepTracker.java
@@ -1,8 +1,32 @@
 package FitTrack.Components;
 
+import java.util.ArrayList;
+
 /**
  * Created by colin on 02/11/16.
  */
 public class sleepTracker
 {
+    private ArrayList<Integer> sleepHistory = new ArrayList<Integer>();
+
+    public sleepTracker( ArrayList<Integer> sleepHistory )
+    {
+        this.sleepHistory = sleepHistory;
+    }
+
+    public ArrayList<Integer> getSleepHistory()
+    {
+        return sleepHistory;
+    }
+
+    public void setSleepHistory( ArrayList<Integer> sleepHistory )
+    {
+        this.sleepHistory = sleepHistory;
+    }
+
+    public void addSleepHistory( int sleepData )
+    {
+
+        this.sleepHistory.add(sleepData);
+    }
 }
