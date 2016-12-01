63291ae089a4707df25720a70a171be36294a609
Colin Beatty <cabeatty@oakland.edu>
2016-12-01 14:26:11 -0500

    Changed the size of the sleep graph popup

--- src/FitTrack/Main/guiMain/Components/sleepGraph.java
+++ src/FitTrack/Main/guiMain/Components/sleepGraph.java
@@ -7,8 +7,6 @@ import FitTrack.Components.sleep;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
-import org.jfree.ui.ApplicationFrame;
-import org.jfree.ui.RefineryUtilities;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 
@@ -18,7 +16,6 @@ import java.util.ArrayList;
 public class sleepGraph extends JFrame
 {
 	ArrayList<sleep> sleepData;
-
 	public sleepGraph(String applicationTitle , String chartTitle, ArrayList<sleep> sleepData)
 	{
 		super(applicationTitle);
@@ -31,17 +28,18 @@ public class sleepGraph extends JFrame
 				true,true,false);
 
 		ChartPanel chartPanel = new ChartPanel( lineChart );
-		chartPanel.setPreferredSize( new java.awt.Dimension( 560 , 367 ) );
+		int raty = (int)(367*1.5);
+		int ratx = (int)(560*1.5);
+		chartPanel.setPreferredSize( new java.awt.Dimension( ratx , raty ) );
 		setContentPane( chartPanel );
 	}
-
 	private DefaultCategoryDataset createDataset( )
 	{
 		DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
 		for (sleep slp : sleepData)
 		{
 			String[] date = slp.getDf().split("/");
-			String dt = date[0] + "-" + date[1];
+			String dt = date[0] + "/" + date[1];
 			dataset.addValue(slp.getHours(), "hours", dt);
 		}
 		return dataset;
